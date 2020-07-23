5114c686568d440860a672e0bbceea3557d4a250
Ben Barber <ben.barber@teamspeak.com>
2020-07-23 11:19:23 +0100

    Added the loading state information

--- app/src/main/java/com/github/binpower93/hilttest/ui/main/PostsViewModel.kt
+++ app/src/main/java/com/github/binpower93/hilttest/ui/main/PostsViewModel.kt
@@ -1,5 +1,7 @@
 package com.github.binpower93.hilttest.ui.main
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import com.github.binpower93.hilttest.core.BaseViewModel
 import com.github.binpower93.hilttest.model.network.PostApi
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -9,8 +11,9 @@ import javax.inject.Inject
 
 class PostsViewModel : BaseViewModel() {
     @Inject lateinit var postApi: PostApi
-
     private lateinit var subscription: Disposable
+
+    val loadingVisibility = MutableLiveData<Int>()
 
     init {
         loadPosts()
@@ -29,11 +32,11 @@ class PostsViewModel : BaseViewModel() {
     }
 
     private fun onRetrievePostListStart() {
-
+        loadingVisibility.value = View.VISIBLE
     }
 
     private fun onRetrievePostListFinish() {
-
+        loadingVisibility.value = View.GONE
     }
 
     private fun onRetrievePostListSuccess() {
