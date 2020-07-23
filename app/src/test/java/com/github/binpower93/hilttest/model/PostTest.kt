b3b077dc666be868e460dc09379524075b6231c4
Ben Barber <ben.barber@teamspeak.com>
2020-07-23 10:42:10 +0100

    Updated tests to cover hashCode and equal checks.

--- app/src/test/java/com/github/binpower93/hilttest/model/PostTest.kt
+++ app/src/test/java/com/github/binpower93/hilttest/model/PostTest.kt
@@ -15,4 +15,36 @@ class PostTest : TestCase() {
         assertEquals("Test Title", post.title)
         assertEquals("Test Content", post.contents)
     }
+
+    fun testThatTwoPostsWithTheSamePropertiesAreEqual() {
+        val post1 = Post(
+            id = "test-id",
+            title = "Test Title",
+            contents = "Test Content"
+        )
+
+        val post2 = Post(
+            id = "test-id",
+            title = "Test Title",
+            contents = "Test Content"
+        )
+
+        assertEquals(post1, post2)
+    }
+
+    fun testThatTwoPostsWithTheSamePropertiesHaveHashCodes() {
+        val post1 = Post(
+            id = "test-id",
+            title = "Test Title",
+            contents = "Test Content"
+        )
+
+        val post2 = Post(
+            id = "test-id",
+            title = "Test Title",
+            contents = "Test Content"
+        )
+
+        assertEquals(post1.hashCode(), post2.hashCode())
+    }
 }
