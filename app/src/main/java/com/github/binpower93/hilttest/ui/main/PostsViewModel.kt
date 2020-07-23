972c8486c1a78b24268c3ed7d748bcd558189456
Ben Barber <ben.barber@teamspeak.com>
2020-07-23 11:17:31 +0100

    Started to work on retrieving Posts from the API

--- app/src/main/java/com/github/binpower93/hilttest/ui/main/PostsViewModel.kt
+++ app/src/main/java/com/github/binpower93/hilttest/ui/main/PostsViewModel.kt
@@ -1,10 +1,51 @@
 package com.github.binpower93.hilttest.ui.main
 
-import androidx.lifecycle.ViewModel
 import com.github.binpower93.hilttest.core.BaseViewModel
 import com.github.binpower93.hilttest.model.network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class PostsViewModel : BaseViewModel() {
     @Inject lateinit var postApi: PostApi
+
+    private lateinit var subscription: Disposable
+
+    init {
+        loadPosts()
+    }
+
+    private fun loadPosts() {
+        subscription = postApi.getPosts()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { onRetrievePostListSuccess() },
+                { onRetrievePostListError() }
+            )
+    }
+
+    private fun onRetrievePostListStart() {
+
+    }
+
+    private fun onRetrievePostListFinish() {
+
+    }
+
+    private fun onRetrievePostListSuccess() {
+
+    }
+
+    private fun onRetrievePostListError() {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
