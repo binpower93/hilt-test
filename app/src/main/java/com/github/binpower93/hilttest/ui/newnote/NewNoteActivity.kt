bce4d4311ed7c064b501b89c27cbb1a8fc170129
Ben Barber <ben.barber@teamspeak.com>
2020-07-27 16:15:59 +0100

    Started to add the new note activity

---
+++ app/src/main/java/com/github/binpower93/hilttest/ui/newnote/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.github.binpower93.hilttest.ui.newnote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.binpower93.hilttest.R
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
---
+++ app/src/main/java/com/github/binpower93/hilttest/ui/newnote/NewNoteViewModel.kt
@@ -0,0 +1,38 @@
+package com.github.binpower93.hilttest.ui.newnote
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.map
+import com.github.binpower93.hilttest.R
+import com.github.binpower93.hilttest.core.BaseViewModel
+import kotlinx.coroutines.flow.combine
+
+class NewNoteViewModel: BaseViewModel() {
+
+    val title = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
+
+    val titleError = title.map {
+        if(it.isNullOrBlank()) {
+            R.string.error_title_needed
+        } else {
+            null
+        }
+    }
+
+    val contentError = content.map {
+        if(it.isNullOrBlank()) {
+            R.string.error_content_needed
+        } else {
+            null
+        }
+    }
+
+    val canSubmit = combine(titleError.asFlow(), contentError.asFlow()) { titleError, contentError ->
+        titleError == null && contentError == null
+    }.asLiveData()
+
+
+
+}
