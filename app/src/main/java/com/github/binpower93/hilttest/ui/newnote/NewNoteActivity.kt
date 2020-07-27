ca2c15aa0ede110e8695a725915185eea7a9c82b
Ben Barber <ben.barber@teamspeak.com>
2020-07-27 17:19:48 +0100

    Should have fixed the odd test case, as the content wasn't correctly connected

--- app/src/main/java/com/github/binpower93/hilttest/ui/newnote/NewNoteActivity.kt
+++ app/src/main/java/com/github/binpower93/hilttest/ui/newnote/NewNoteActivity.kt
@@ -2,11 +2,23 @@ package com.github.binpower93.hilttest.ui.newnote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.github.binpower93.hilttest.R
+import com.github.binpower93.hilttest.databinding.NewNoteActivityBinding
 
 class NewNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: NewNoteActivityBinding
+    private lateinit var viewModel: NewNoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_note)
+        setContentView(R.layout.new_note_activity)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.new_note_activity)
+        binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this).get(NewNoteViewModel::class.java)
+        binding.viewModel = viewModel
     }
 }
